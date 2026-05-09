7b4d8c1c07f1e5569bcdf1eedfeaae31eceaf0f6
reni165-debug <231001165@rajalakshmi.edu.in>
2026-05-09 23:51:42 +0530

    Fixed Render startup

--- app.py
+++ app.py
@@ -981,4 +981,4 @@ if __name__ == "__main__":
     print("=" * 55)
     print("  Smart Queue System  |  http://localhost:5000")
     print("=" * 55)
-    app.run(debug=True, use_reloader=False, port=5000)
+    app.run(debug=True, use_reloader=False, port=5000,host="0.0.0.0")
